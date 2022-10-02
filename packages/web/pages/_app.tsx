5315b40f57291e31c1b20028846150375e7926d4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-10-02 14:31:48 +0300

    Add HomeContextProvider to _app

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -4,15 +4,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import HomeContextProvider from '../contexts/HomeContext/HomeContextProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools />
-      <ToastContainer />
+      <HomeContextProvider>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+        <ToastContainer />
+      </HomeContextProvider>
     </QueryClientProvider>
   );
 }
