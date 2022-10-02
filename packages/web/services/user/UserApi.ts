3e989fe23a9333025c95918679ce995781ff5b56
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-10-02 16:47:31 +0300

    Update UserApi

--- packages/web/services/user/UserApi.ts
+++ packages/web/services/user/UserApi.ts
@@ -1,5 +1,6 @@
 import { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
 import { createApi, GenericServiceError, User } from '../common';
+import { UpdateUserDto } from './dto';
 
 export class UserApi {
   private api: AxiosInstance;
@@ -11,6 +12,14 @@ export class UserApi {
   public async getCurrentUser(): Promise<AxiosResponse<User> | AxiosError<GenericServiceError>> {
     try {
       return await this.api.get<User>('/user/me');
+    } catch (e) {
+      return e as AxiosError<GenericServiceError>;
+    }
+  }
+
+  public async updateUser(dto: UpdateUserDto): Promise<AxiosResponse<User> | AxiosError<GenericServiceError>> {
+    try {
+      return await this.api.patch('/user', dto);
     } catch (e) {
       return e as AxiosError<GenericServiceError>;
     }
